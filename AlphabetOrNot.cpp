dae9d1fe8d193e031b49a005eae99ad11de3f1c4
SHARMILA DEVI A S <113982981+sharmiladharsh@users.noreply.github.com>
2025-02-21 15:26:35 +0530

    Add files via upload

---
+++ AlphabetOrNot.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char character='8';
+    if(isalpha(character)){
+        cout<<"It is an Alphabet";
+    }
+    else{
+        cout<<"It is Not a Alphabet";
+    }
+}
